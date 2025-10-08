836f255c9ca8ee31de95fa76baa7237815e1c428
arvind-rana <arvindrana8650@gmail.com>
2025-10-08 20:43:31 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,36 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//       remotePatterns: [
+//         {
+//           protocol: "https",
+//           hostname: "images.unsplash.com",
+//         },
+//       ],
+//     },
+//   };
+  
+//   export default nextConfig;
+  
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      { protocol: "https", hostname: "ik.imagekit.io" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+  
+    ],
+  },
+};
 
 export default nextConfig;
